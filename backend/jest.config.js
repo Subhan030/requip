4cf1048d69658f981e8c564847f686b9057b958c
Subhan Kumar Rai <subhankumarrai@gmail.com>
2026-06-21 09:41:00 +0530

    add backend package.json and typescript config

---
+++ backend/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/index.ts',
+  ],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testTimeout: 10000,
+};
